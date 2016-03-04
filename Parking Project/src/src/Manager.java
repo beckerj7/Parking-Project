7c483eb0bd86e1b688aaebc5fd791b2081c18b0e
Brandon Koury <Brandon Koury@BrandonKoury-PC>
2016-03-04 18:49:40 -0500

    spots taken/Available

--- Parking Project/src/src/Manager.java
+++ Parking Project/src/src/Manager.java
@@ -33,6 +33,7 @@ public class Manager extends Application
 	VBox VBoxGraph;
 
 	int spots = display.spotsAvailable;
+	int taken = display.spotsTaken;
 	String imageLocation;
 	
 	
@@ -83,7 +84,7 @@ public class Manager extends Application
 		ImageView.setFitWidth(800);//imageView formatting
 		ImageView.setPreserveRatio(true);
 
-		taDisplay.setText("Number of parking spots available: " + spots);//set text to be displayed
+		taDisplay.setText("Number of parking spots available: " + spots + "\nNumber of parking spots Taken: " + taken);//set text to be displayed
 
 		for (i=0; i<7; i++) Plots[i].Plot(i, this);//create test dataplot for GUI
 		HBoxBt.getChildren().addAll(btLeft, btRight);
