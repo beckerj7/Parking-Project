2635dbb93a3da5451f0b60b8291448c732d24b03
Brandon Koury <Brandon Koury@BrandonKoury-PC>
2016-03-04 18:30:19 -0500

    Deleted DateAndTime

--- Parking Project/src/src/display.java
+++ Parking Project/src/src/display.java
@@ -1,6 +1,12 @@
 package src;
 
+/**
+ * @author Brandon Koury
+ *
+ */
 public class display {
+	//creates variable spotsAvailable with assigned value
 	public static int spotsAvailable = 13;
+	
 
 }
